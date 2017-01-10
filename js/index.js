32b798758d66116915505f3a989b97a275fd0d9e
kaptenfest <kaptenfest@gmail.com>
2017-01-10 09:05:08 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// StyleSheets
+require("../css/00-global.css");
+
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components / Containers
+import Test from './components/test';
+
+const app = document.getElementById("app");
+
+ReactDOM.render(
+	<Test />,
+app
+
+);
