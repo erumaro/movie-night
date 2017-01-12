7b3addd9e2e41e5878926f55896f3cc307df2612
kaptenfest <kaptenfest@gmail.com>
2017-01-12 13:59:55 +0100

    boilerplate etc

--- js/index.js
+++ js/index.js
@@ -5,13 +5,31 @@ require("../css/00-global.css");
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Redux
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from "redux-promise";
+// import reducers from './reducers';
+
+// Redux Logger
+import createLogger from 'redux-logger';
+
+// React Router
+// import {Router, Route, IndexRoute, Link, IndexLink, IndexRedirect, hashHistory} from 'react-router';
+
 // Components / Containers
 import Test from './components/test';
+
+// Consts
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise, logger)(createStore);
 
 const app = document.getElementById("app");
 
 ReactDOM.render(
-	<Test />,
+  // <Provider store={createStoreWithMiddleware(reducers)}>
+		<Test />,
+	// </Provider>,
 app
 
 );
