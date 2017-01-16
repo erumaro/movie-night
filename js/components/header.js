25faffa22d2f9a411e3499f865f33a17b52b2a4e
Tobias Årud <me@tobiasarud.se>
2017-01-16 14:34:20 +0100

    början till menyn

---
+++ js/components/header.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class Header extends Component {
+  render(){
+    return(
+      <header>
+        <img src="../img/logo.PNG" />
+        <nav className="site-navigation">
+          <ul>
+            <li>Film</li>
+              <ul>
+                <li className="menu-item">
+                  <Link to="...">
+                    Popular
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="...">
+                    Now Playing
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="...">
+                    Top Rated
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="...">
+                    Upcoming
+                  </Link>
+                </li>
+              </ul>
+            <li>TV</li>
+              <ul>
+                <li className="menu-item">
+                  <Link to="...">
+                    Popular
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="...">
+                    Now Playing
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="...">
+                    Top Rated
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="...">
+                    Upcoming
+                  </Link>
+                </li>
+              </ul>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
