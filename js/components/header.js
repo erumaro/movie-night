34e3705f436bc7a49bc3c30e7cface849160e7b0
Tobias Årud <me@tobiasarud.se>
2017-01-22 21:28:43 +0100

    fungerander hover version tillsvidare

--- js/components/header.js
+++ js/components/header.js
@@ -12,61 +12,65 @@ export default class Header extends Component {
               <li>
                 <Link to="/movieOverview/#">
                   Film
+                  <div className="sub-menu">
+                      <div className="centeringDiv">
+                          <ul>
+                            <li className="menu-item">
+                              <Link to="/movieOverview/popular">
+                                Popular
+                              </Link>
+                            </li>
+                            <li className="menu-item">
+                              <Link to='/movieOverview/playing'>
+                                Now Playing
+                              </Link>
+                            </li>
+                            <li className="menu-item">
+                              <Link to="/movieOverview/topRated">
+                                Top Rated
+                              </Link>
+                            </li>
+                            <li className="menu-item">
+                              <Link to="/movieOverview/upcoming">
+                                Upcoming
+                              </Link>
+                            </li>
+                          </ul>
+                      </div>
+                </div>
                 </Link>
               </li>
-              <div className="sub-menu">
-              <div className="centeringDiv">
-              <ul>
-                <li className="menu-item">
-                  <Link to="/movieOverview/popular">
-                    Popular
-                  </Link>
-                </li>
-                <li className="menu-item">
-                  <Link to='/movieOverview/playing'>
-                    Now Playing
-                  </Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/movieOverview/topRated">
-                    Top Rated
-                  </Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/movieOverview/upcoming">
-                    Upcoming
-                  </Link>
-                </li>
-              </ul>
-              </div>
-            </div>
               <li>
                 <Link to="/tvOverview/#">
                   TV
+                    <div className="sub-menu">
+                      <div className="centeringDiv">
+                          <ul>
+                            <li className="menu-item">
+                              <Link to="/tvOverview/popular">
+                                Popular
+                              </Link>
+                            </li>
+                            <li className="menu-item">
+                              <Link to="/tvOverview/playing">
+                                Now Playing
+                              </Link>
+                            </li>
+                            <li className="menu-item">
+                              <Link to="/tvOverview/topRated">
+                                Top Rated
+                              </Link>
+                            </li>
+                            <li className="menu-item">
+                              <Link to="/tvOverview/upcoming">
+                                Upcoming
+                              </Link>
+                            </li>
+                          </ul>
+                      </div>
+                </div>
                 </Link>
               </li>
-              <ul>
-                <li className="menu-item">
-                  <Link to="/tvOverview/popular">
-                    Popular
-                  </Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/tvOverview/playing">
-                    Now Playing
-                  </Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/tvOverview/topRated">
-                    Top Rated
-                  </Link>
-                </li>
-                <li className="menu-item">
-                  <Link to="/tvOverview/upcoming">
-                    Upcoming
-                  </Link>
-                </li>
-              </ul>
           </ul>
         </nav>
         </div>
