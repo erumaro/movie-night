d48a5bdfdb02b75dc64e8b976d2c894e20e8dae6
Niklas Lindberg <dorkinacage@gmail.com>
2017-01-24 12:47:18 +0100

    tests for movie actions

--- __tests__/movie_detail_reducer_test.js
+++
@@ -1,9 +0,0 @@
-import requestMovieDetails from '../js/reducers/movie_details_reducer';
-
-describe('movie details reducer', () => {
-	it('Has default state', () => {
-		expect(requestMovieDetails('THE_WRONG_TYPE')).toEqual({
-			state:[]
-		});
-	});
-});
