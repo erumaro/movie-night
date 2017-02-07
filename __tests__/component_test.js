341c26b2dc2814e90b8cb37425439fa04370fd20
kaptenfest <kaptenfest@gmail.com>
2017-02-07 09:23:48 +0100

    latest

--- __tests__/component_test.js
+++ __tests__/component_test.js
@@ -7,33 +7,17 @@ import { MovieDetails } from '../js/containers/movie_details';
 import configureMockStore from 'redux-mock-store';
 import axios from 'axios';
 import * as types from "../js/constants/ActionTypes.js";
-const middlewares = [axios] 
-
 
 describe('components', function() {
   describe('<MovieDetails />', function() {
-  	function success() {
-	  return {
-	    type: 'FETCH_MOVIE_DETAILS'
-	  }
-	}
 
-	function fetchData () {
-	  return dispatch => {
-	    return fetch('../test_mocks/fetch_movie_details.json') // Some async action with promise
-	      .then(() => dispatch(success()))
-	  };
-	}
-   	it('should execute fetch data', () => {
-  		const store = mockStore({})
+    it('Link renders correctly', () => {
+  const tree = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  		// Return the promise
- 		 return store.dispatch(fetchData())
-    	.then(() => {
-	      const actions = store.getActions()
-	      expect(actions[0]).toEqual(success())
-      })
-	})
   });
 });
 
